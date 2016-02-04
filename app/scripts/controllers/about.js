1558f85a61d651dc2a68bd7cbaa51807ae0dc880
Bob Roberts <broberts_az@yahoo.com>
2016-02-04 00:03:51 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name megaanalyticsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the megaanalyticsApp
+ */
+angular.module('megaanalyticsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name megaanalyticsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the megaanalyticsApp
+ */
+angular.module('megaanalyticsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
